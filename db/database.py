44a224f6aa227987c04757f30ec063405f67d362
vazhap519 <phankvelashvili.mail@gmail.com>
2026-05-17 13:12:36 +0400

    change db

--- db/database.py
+++ db/database.py
@@ -1,9 +1,22 @@
-import os
+# import os
+# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+# from sqlalchemy.orm import sessionmaker
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# engine = create_async_engine(os.getenv("DATABASE_URL"))
+# SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 
-load_dotenv()
+DATABASE_URL = "sqlite+aiosqlite:///database.db"
 
-engine = create_async_engine(os.getenv("DATABASE_URL"))
-SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+engine = create_async_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
